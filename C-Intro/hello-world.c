259ba38fab4489fd648ab2aa0d178fc186af8d9a
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-04-22 02:03:20 +0800

    ran: simple program

--- C-Intro/hello-world.c
+++ C-Intro/hello-world.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
-int main() {
-  printf("To C, or not to C, that is the question. \n");
-  return 0;
-}
+ int main() {
+    printf("Brevity is the soul of wit. \n");
+    return 0;
+ }
 
 /*This is a comment!*/
 /*A Multiline Comment would actually go great too!
