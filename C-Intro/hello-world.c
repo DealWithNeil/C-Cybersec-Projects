7e5e8a4733d110139376c7a2a24235bae6571f2f
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-04-26 23:34:38 +0800

    ran: blank line using spaces

--- C-Intro/hello-world.c
+++ C-Intro/hello-world.c
@@ -1,12 +1,14 @@
 /*I would like to say that i think i have done pretty well on starting with the basics of C!*/
 
 #include <stdio.h>
- int main() {
-   printf("hello world! \n");
-   printf("have a blessed day ahead! \n");
-   printf("be the best version of yourself today! \n");
-    return 0;
- }
+
+int main() {
+
+  printf("Hello, World!\n\n");
+  printf("I am Learning C!");
+
+
+}
 
 /*This is a comment!*/
 /*A Multiline Comment would actually go great too!
