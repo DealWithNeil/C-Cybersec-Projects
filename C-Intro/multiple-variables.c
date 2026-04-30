04ebe554355a8a01e40d2dc233735ed1b2e48b79
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-04-30 16:32:35 +0800

    ran: multiple variable program

--- C-Intro/multiple-variables.c
+++ C-Intro/multiple-variables.c
@@ -1,1 +1,12 @@
 /*I will now create a simple program with multiple variables*/
+
+#include <stdio.h>
+
+int main (void ) {
+
+    int     x, y, z, sum;
+    x = y = z = 50;
+    sum = x + y + z;
+    printf("%d", sum);
+
+}
