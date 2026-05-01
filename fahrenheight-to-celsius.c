8ed680294d04a86e88b0da9546f71b3e59fb08d4
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-02 01:22:26 +0800

    ran: line break code

--- fahrenheight-to-celsius.c
+++ fahrenheight-to-celsius.c
@@ -1,8 +1,19 @@
+/* Condense the dweight.c program by (1) replacing the assignments to height,
+length, and width with initializers and (2) removing the weight variable, instead 
+calculating (volume + 165) / 166 within the last printf. /*/
+
 #include <stdio.h>
-int main(void)
-{
-  printf("Parkinson's Law:\nWork expands so as to ");
-  printf("fill the time\n");
-  printf("available for its completion.\n");
+
+int main(void) {
+
+  int height = 8;
+  int length = 12;
+  int width = 10;
+
+  volume = height * length * width;
+  (volume + 165) / 166;
+  printf("Dimensions: %dx%dx%d\n", length, width, height);
+  printf("Volume (cubic inches): %d\n", volume);
+
   return 0;
 }
