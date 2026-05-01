d5c1c9894a19d8de0e040a0622096d4b8fcfb47c
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-01 18:03:18 +0800

    file: programming exercise multiple line break

--- fahrenheight-to-celsius.c
+++ fahrenheight-to-celsius.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-  printf("hello, world\n");
+  printf("Parkinson's Law:\nWork expands so as to ");
+  printf("fill the time\n");
+  printf("available for its completion.\n");
+  return 0;
 }
