4f1e92b300a239c4d1e51e4fd473dd9a1653e937
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-03 21:33:09 +0800

    ran: multiple int and float variables

--- several-int-and-float-variables.c
+++ several-int-and-float-variables.c
@@ -3,7 +3,10 @@
 int main (void) {
 
     int x = 10;
-    
+    float y = 20;
+    int z = 6;
+    float a = 7;
 
-
+    printf("x = %d, y = %.2f, z = %d, a = %.2f\n", x, y, z, a);
+    return 0;
 }
