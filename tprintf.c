bb3eafc7d50202db921feb5b844254b722eb5268
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-05 00:56:40 +0800

    fix: syntax errors

--- tprintf.c
+++ tprintf.c
@@ -7,11 +7,11 @@
         int i;
         float x;
 
-        i 40;
+        i =40;
         x = 839.21f;
 
         printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
         printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
 
-        reurn 0;
+        return 0;
     }
