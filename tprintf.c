316f886197406c1b53cc9697453453b425ac53d7
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-06 01:28:42 +0800

    ran: simple program for scanf with escape sequences

--- tprintf.c
+++ tprintf.c
@@ -1,17 +1,10 @@
-/*Prints int and float values in various formats*/
+/*scanf function with escape sequences*/
 
-#include<stdio.h>
+#include <stdio.h>
 
-    int main (void) {
+int main(void) {
 
-        int i;
-        float x;
-
-        i =40;
-        x = 839.21f;
-
-        printf("|%d|%5d|%-5d|%5.3d|\n", i, i, i, i);
-        printf("|%10.3f|%10.3e|%-10g|\n", x, x, x);
-
-        return 0;
-    }
+    printf("Item\tUnit\tPurchase\n\tPrice\tDate\n");  
+    
+    return 0;
+}
