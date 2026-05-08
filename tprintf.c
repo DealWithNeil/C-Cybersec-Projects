66807255bc918d02279945bbf135ca014226cdd4
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-08 13:51:52 +0800

    ran: book programming exercise

--- tprintf.c
+++ tprintf.c
@@ -1,22 +1,8 @@
-/*Adding Fractions using Scanf*/
+/*Book Programming Exercise*/
 
 #include <stdio.h>
 
-int main(void) {
+int main (void) {
 
-    int num1, denom1, num2, denom2, result_num, result_denom;
-
-     printf("Enter first fraction: ");
-
-    scanf("%d/%d", &num1, &denom1);
-
-    printf("Enter second fraction: ");
-    scanf("%d/%d", &num2, &denom2);
-
-    result_num = num1 * denom2 + num2 * denom1;
-    result_denom = denom1 * denom2;
-    printf("The sum is %d/%d\n", result_num, result_denom);
-
-    return 0;
-
+     printf("%6d,%4d", 86, 1040);
 }
