0f04b78456df3bea8844cad5e87b653de73b19fb
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-21 00:28:13 +0800

    Create: nested if-else program with multiple conditions

--- unarysum.c
+++ unarysum.c
@@ -1,18 +1,29 @@
-/*Displaying a nested if-else statement with unary operators */
+/*Displaying a nested if-else statement with multiple conditions */
+
 
 #include <stdio.h>
 
-int main() {
-    int a = 5, b = 10;
+    int maint (void) {
 
-    if (a > b) {
-        printf("a is greater than b\n");
-    } else if (a < b) {
-        printf("a is less than b\n");
-    } else {
-        printf("a is equal to b\n");
+            int a, b, c;
+    
+            printf("Enter three integers: ");
+            scanf("%d %d %d", &a, &b, &c);
+    
+            if (a > b) {
+                if (a > c) {
+                    printf("%d is the largest number.\n", a);
+                } else {
+                    printf("%d is the largest number.\n", c);
+                }
+            } else {
+                if (b > c) {
+                    printf("%d is the largest number.\n", b);
+                } else {
+                    printf("%d is the largest number.\n", c);
+                }
+            }
+
+        return 0;
     }
-
-    return 0;
-}
 
