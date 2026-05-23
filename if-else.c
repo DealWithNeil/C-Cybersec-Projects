692a99c95eb674a6791573a868526e048ac1eb3d
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-24 05:30:18 +0800

    Create: nested if-else

--- if-else.c
+++ if-else.c
@@ -1,9 +1,10 @@
-/*Displaying a multiple nested if-else statements with multiple conditions and operators */
+/*Displaying a multiple nested if-else statements with multiple operators */
 
 
 #include <stdio.h>
 
     int main () {
+
         int a = 10, b = 20, c = 30;
         if (a > b && a > c) {
             printf("a is the greatest number");
@@ -17,6 +18,7 @@
         else {
             printf("All numbers are equal");
         }
+
 
         return 0;
     }
