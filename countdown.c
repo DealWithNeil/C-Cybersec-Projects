70a716c97c593b44f4b7fb7b548bafa5d764d838
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-30 01:38:33 +0800

    update: countdown program with funny messages

--- countdown.c
+++ countdown.c
@@ -1,14 +1,13 @@
-/*Basic Countdown Program*/
+/*Basic Countdown Program with funny messages*/
 
 #include <stdio.h>
 
-int main(void)  {
-
+int main(void) {
     int i = 10;
     while (i > 0) {
-    printf("T minus %d and counting\n", i);
-    i--;
-
+        printf("T minus %d and counting\n", i);
+        i--;
     }
+    printf("Liftoff!\n");
     return 0;
 }
