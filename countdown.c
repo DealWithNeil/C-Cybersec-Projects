1020c1f1c5ae19838f1a1b2d8bd75c705d434498
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-05-31 01:35:49 +0800

    Create: countdown with many funny messages

--- countdown.c
+++ countdown.c
@@ -1,13 +1,19 @@
-/*Basic Countdown Program with funny messages*/
+/*Basic Countdown Program with many funny messages*/
 
 #include <stdio.h>
+#include <unistd.h>
 
-int main(void) {
-    int i = 10;
-    while (i > 0) {
-        printf("T minus %d and counting\n", i);
-        i--;
+int main() {
+    int count = 10;
+
+    while (count > 0) {
+        printf("%d...\n", count);
+        sleep(1); // Sleep for 1 second
+        count--;
     }
-    printf("Liftoff!\n");
+
+    printf("Blast off! 🚀\n");
+    printf("Hope you enjoyed the countdown! 🎉\n");
     return 0;
 }
+
