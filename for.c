522f48b18fe95e0d867ee351ce5c788a0e69dec8
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-06-04 00:03:30 +0800

    Create: basic for loop program

---
+++ for.c
@@ -0,0 +1,12 @@
+/*Print a basic for loop program*/
+
+#include <stdio.h>
+
+int main (void) {
+
+    int i;
+    for (i = 10; i > 0; i--) 
+    printf("T minus %d and counting\n", i);
+    
+    return 0;
+}
