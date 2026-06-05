34c9a9ba4e6e4695cf6b542080d844d35817b362
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-06-06 00:17:06 +0800

    Update: prints a table of squares using for

--- for.c
+++ for.c
@@ -1,13 +1,12 @@
-/*Print a basic for loop program*/
-
+/* Prints a table of squares using a for statement */
 #include <stdio.h>
-
-int main (void) {
-
-    int i;
-    for (i = 10; i > 0; i--)
-        while (i > 5) 
-    printf("T minus %d and counting\n", i);
-    
-    return 0;
+int main(void)
+{
+  int i, n;
+  printf("This program prints a table of squares.\n");
+  printf("Enter number of entries in table: ");
+  scanf("%d", &n);
+  for (i = 1; i <= n; i++)
+    printf("%10d%10d\n", i, i * i);
+  return 0;
 }
