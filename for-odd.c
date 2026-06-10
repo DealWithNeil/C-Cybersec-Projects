115bb20b22c2efb6ce2db182cc0d5ca3a4bbf41b
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-06-11 00:21:19 +0800

    update: balancing a checkbook

--- for-odd.c
+++ for-odd.c
@@ -1,17 +1,32 @@
-/*Display a basic for loop with go to*/
+/*Balancing a checkbook*/
 
 #include <stdio.h>
 
 int main(void)
 {
-    int i = 1;
-
-    loop:
-        printf("%d ", i);
-        i += 2;
-        if (i <= 10)
-            goto loop;
-
-    printf("\n");
+    char command;
+    
+    for (;;) {
+        printf("Enter command: ");
+        
+        // Note the space before %c: This skips newline characters (\n) left in the buffer 
+        // when you hit Enter, preventing the loop from printing "Enter command:" twice.
+        scanf(" %c", &command); 
+        
+        switch (command) {
+            case 'a': 
+                printf("Performing operation 1...\n"); 
+                break;
+                
+            case 'q':
+                printf("Exiting program.\n");
+                return 0; // Gracefully breaks the infinite loop and ends the program
+                
+            default:
+                printf("Unknown command. Try again.\n");
+                break;
+        }
+    } // End of for loop
+    
     return 0;
 }
