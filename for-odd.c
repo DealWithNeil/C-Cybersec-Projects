f4995135f6f3ee0d4638dc0337259c4ed6e3c62e
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-06-13 01:20:18 +0800

    update: for-odd

--- for-odd.c
+++ for-odd.c
@@ -1,49 +1,18 @@
-/*Balancing a checkbook with multiple switch cases*/
-
+/* Prints a table of squares using an odd method */
 #include <stdio.h>
+int main(void)
+{
+  int i, n, odd, square;
+  printf("This program prints a table of squares.\n");
+  printf("Enter number of entries in table: ");
+  scanf("%d", &n);
+  i = 1;
+  odd = 3;
+  for (square = 1; i <= n; odd += 2) {
+    printf("%10d%10d\n", i, square);
+    ++i
+    }
+    square += odd;
 
-int main(void) {
-    int choice;
-    double balance = 0.0, amount;
-    
-    printf("Welcome to the checkbook balancing program!\n");
-    
-    do {
-        printf("\nPlease select an option:\n");
-        printf("1. Deposit\n");
-        printf("2. Withdraw\n");
-        printf("3. Check Balance\n");
-        printf("4. Exit\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
-        
-        switch (choice) {
-            case 1:
-                printf("Enter the amount to deposit: ");
-                scanf("%lf", &amount);
-                balance += amount;
-                printf("Deposit successful! New balance: $%.2lf\n", balance);
-                break;
-            case 2:
-                printf("Enter the amount to withdraw: ");
-                scanf("%lf", &amount);
-                if (amount > balance) {
-                    printf("Insufficient funds! Withdrawal failed.\n");
-                } else {
-                    balance -= amount;
-                    printf("Withdrawal successful! New balance: $%.2lf\n", balance);
-                }
-                break;
-            case 3:
-                printf("Current balance: $%.2lf\n", balance);
-                break;
-            case 4:
-                printf("Thank you for using the checkbook balancing program. Goodbye!\n");
-                break;
-            default:
-                printf("Invalid choice! Please try again.\n");
-        }
-    } while (choice != 4); // <-- FIXED: Added the missing closing bracket and loop condition
-    
     return 0;
 }
