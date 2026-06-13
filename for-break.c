902d1694de9e0e0ee814e8e3c6bd51f2ec98a36f
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-06-14 01:05:09 +0800

    create: for-break

---
+++ for-break.c
@@ -0,0 +1,16 @@
+/*Display a simple program with the for loop and break statement*/
+
+#include<stdio.h>
+
+int main(void)
+{
+  int n;    
+for (;;) {
+  printf("Enter a number (enter 0 to stop): ");
+  scanf("%d", &n);
+  if (n == 0)
+    break;
+  printf("%d cubed is %d\n", n, n * n * n);
+}
+return 0;
+}
