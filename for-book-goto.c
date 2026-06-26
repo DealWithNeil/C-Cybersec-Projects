14234f517bb8218a80c1c198a7c0b510826e2690
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-06-27 02:21:26 +0800

    chore: create for book goto exercise

---
+++ for-book-goto.c
@@ -0,0 +1,17 @@
+/*Show how to replace a continue statement by an equivalent goto statement.*/
+
+#include <stdio.h>
+
+int main (void) {
+
+    for (int i = 1; i <= 10; i++) {
+        if (i % 2 == 0) {
+            goto skip; // Replace continue with goto
+        }
+        printf("%d ", i);
+        skip:; // Label to jump to
+    }
+    
+
+    return 0;
+}
