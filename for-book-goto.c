33c4eb6da40401c4ed6b2406e6f74cae9445f46d
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-05 02:49:41 +0800

    chore: update block of code for new exercise

--- for-book-goto.c
+++ for-book-goto.c
@@ -6,5 +6,13 @@ between 1 and n*/
 
 int main() {
     
+int n, i;
+printf("Enter a number: ");
+scanf("%d", &n);
+for (i = 2; i <= n; i += 2) {
+        printf("%d ", i * i);
+    }
+    printf("\n");
+    
     return 0;
 }
