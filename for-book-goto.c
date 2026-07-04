6832cf1204f820904cd757f52949c574318d95bb
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-05 02:48:10 +0800

    chore: remove block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -5,24 +5,6 @@ terms*/
 #include <stdlib.h> // For abs() or custom checks
 
 int main() {
-    int numerator, denominator, gcd;
-    printf("Enter a fraction (numerator/denominator): ");
-    scanf("%d/%d", &numerator, &denominator);
-
-    // Calculate GCD using Euclidean algorithm
-    int a = abs(numerator);
-    int b = abs(denominator);
-    while (b != 0) {
-        int temp = b;
-        b = a % b;
-        a = temp;
-    }
-    gcd = a;
-
-    // Reduce the fraction
-    numerator /= gcd;
-    denominator /= gcd;
-
-    printf("The reduced fraction is: %d/%d\n", numerator, denominator);
+    
     return 0;
 }
