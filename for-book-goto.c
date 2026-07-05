4cc6f0b43e087a9e774b617d30af20a9e21bef0a
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-06 00:38:30 +0800

    chore: update block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -4,7 +4,28 @@ the month and the day of the week on which the month begins*/
 #include <stdio.h>
 
 int main() {
-    
+    int days, start_day;
+    int i, j;
+
+    printf("Enter the number of days in the month: ");
+    scanf("%d", &days);
+    printf("Enter the day of the week the month begins (0=Sunday, 1=Monday, ..., 6=Saturday): ");
+    scanf("%d", &start_day);
+
+    printf(" Sun Mon Tue Wed Thu Fri Sat\n");
+
+    for (i = 0; i < start_day; i++) {
+        printf("    ");
+    }
+
+    for (i = 1; i <= days; i++) {
+        printf("%3d ", i);
+        if ((i + start_day) % 7 == 0) {
+            printf("\n");
+        }
+    }
+
+    printf("\n");
 
     return 0;
 }
