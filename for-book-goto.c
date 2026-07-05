a3d0e5f26d38711bb4c89f7e1d776330adf2a974
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-06 00:37:13 +0800

    chore: remove old block of code from recent exercise

--- for-book-goto.c
+++ for-book-goto.c
@@ -5,13 +5,6 @@ the month and the day of the week on which the month begins*/
 
 int main() {
     
-int n, i;
-printf("Enter a number: ");
-scanf("%d", &n);
-for (i = 2; i <= n; i += 2) {
-        printf("%d ", i * i);
-    }
-    printf("\n");
 
     return 0;
 }
