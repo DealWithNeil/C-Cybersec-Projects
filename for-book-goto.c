2667742ae439bcb2e2b92e91836e535938057c14
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-07 01:55:29 +0800

    chore: remove code for recent exercise

--- for-book-goto.c
+++ for-book-goto.c
@@ -9,26 +9,7 @@ balance remaining after each of these payments*/
 #include <stdio.h>
 
 int main() {
-    int days, start_day;
-    int i, j;
-
-    printf("Enter the number of days in the month: ");
-    scanf("%d", &days);
-    printf("Enter the day of the week the month begins (0=Sunday, 1=Monday, ..., 6=Saturday): ");
-    scanf("%d", &start_day);
-
-    printf(" Sun Mon Tue Wed Thu Fri Sat\n");
-
-    for (i = 0; i < start_day; i++) {
-        printf("    ");
-    }
-
-    for (i = 1; i <= days; i++) {
-        printf("%3d ", i);
-        if ((i + start_day) % 7 == 0) {
-            printf("\n");
-        }
-    }
+    
 
     printf("\n");
 
