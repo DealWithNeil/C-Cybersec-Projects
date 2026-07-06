2443df8b367d275c1b177348a98774fa06dc144c
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-07 01:55:07 +0800

    chore: update commented code of recent exercise

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,6 +1,11 @@
 /*  Write a program that prints a one-month calendar. The user specifies the number of days in
 the month and the day of the week on which the month begins*/
 
+/*Programming Project 8 in Chapter 2 asked you to write a program that calculates the
+remaining balance on a loan after the first, second, and third monthly payments. Modify the
+program so that it also asks the user to enter the number of payments and then displays the
+balance remaining after each of these payments*/
+
 #include <stdio.h>
 
 int main() {
