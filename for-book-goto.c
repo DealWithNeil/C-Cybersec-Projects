58eeb23d3e2cc200671c864ddf58dabd47689631
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-08 07:50:09 +0800

    chore: remove first block of comment

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,5 +1,4 @@
-/*  Write a program that prints a one-month calendar. The user specifies the number of days in
-the month and the day of the week on which the month begins*/
+
 
 /*Programming Project 8 in Chapter 2 asked you to write a program that calculates the
 remaining balance on a loan after the first, second, and third monthly payments. Modify the
