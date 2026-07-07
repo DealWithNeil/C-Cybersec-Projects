bc76db66afe73e6c8049346503d4868c763c5b4e
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-08 07:51:56 +0800

    chore: add new block of code for newer exercise

--- for-book-goto.c
+++ for-book-goto.c
@@ -7,8 +7,16 @@ where n is an integer entered by the user.*/
 #include <stdio.h>
 
 int main() {
-    
+    int n, i;
+    double e = 1.0, factorial = 1.0;
+    printf("Enter an integer n to approximate e: ");
+    scanf("%d", &n);
 
+    for (i = 1; i <= n; i++) {
+        factorial *= i; // Calculate i!
+        e += 1.0 / factorial; // Add 1/i! to e
+    }
+    printf("Approximation of e using n = %d: %.10f\n", n, e);
 
     return 0;
 }
