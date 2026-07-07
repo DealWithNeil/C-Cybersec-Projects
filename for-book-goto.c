cdb9048e71e9bed77eec13cc3b718fb4f9026a02
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-08 07:51:11 +0800

    chore: remove old block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -8,29 +8,7 @@ where n is an integer entered by the user.*/
 
 int main() {
     
-int days, start_day, i, j;
 
-    printf("Enter the number of days in the month: ");
-    scanf("%d", &days);
-
-    printf("Enter the starting day of the week (1=Sunday, 2=Monday, ..., 7=Saturday): ");
-    scanf("%d", &start_day);
-
-    printf("\nSun Mon Tue Wed Thu Fri Sat\n");
-
-    for (i = 1; i < start_day; i++) {
-        printf("    ");
-    }
-
-    for (j = 1; j <= days; j++) {
-        printf("%3d ", j);
-        if ((j + start_day - 1) % 7 == 0) {
-            printf("\n");
-        }
-    }
-
-    
-    printf("\n");
 
     return 0;
 }
