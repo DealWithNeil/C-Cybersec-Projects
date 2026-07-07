edf86a6ef716321f592ac719cead3880175ba2ce
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-08 07:50:41 +0800

    chore: update commented code

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,9 +1,8 @@
-
-
-/*Programming Project 8 in Chapter 2 asked you to write a program that calculates the
-remaining balance on a loan after the first, second, and third monthly payments. Modify the
-program so that it also asks the user to enter the number of payments and then displays the
-balance remaining after each of these payments*/
+/*The value of the mathematical constant e can be expressed as an infinite series:
+e = 1 + 1/1! + 1/2! + 1/3! + …
+Write a program that approximates e by computing the value of
+1 + 1/1! + 1/2! + 1/3! + … + 1/n!
+where n is an integer entered by the user.*/
 
 #include <stdio.h>
 
