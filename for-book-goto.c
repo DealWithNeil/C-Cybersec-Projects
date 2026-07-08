91f9aa96aeb8def715f579261eaed5f69d5756c6
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-09 03:47:31 +0800

    chore: save updated code

--- for-book-goto.c
+++ for-book-goto.c
@@ -6,19 +6,26 @@ where n is an integer entered by the user.*/
 
 /*Modify the program so that the program continues adding terms until the current term becomes less than ε, where ε is a small (floating-point) number entered by the user.
 */
+
 #include <stdio.h>
 
 int main() {
     int n, i;
-    double e = 1.0, factorial = 1.0;
+    double e = 1.0, factorial = 1.0, epsilon;
     printf("Enter an integer n to approximate e: ");
     scanf("%d", &n);
+    printf("Enter the tolerance epsilon: ");
+    scanf("%lf", &epsilon);
 
-    for (i = 1; i <= n; i++) {
+    double term = 1.0;
+    i = 1;
+    while (term >= epsilon) {
         factorial *= i; // Calculate i!
-        e += 1.0 / factorial; // Add 1/i! to e
+        term = 1.0 / factorial; // Calculate the current term
+        e += term; // Add the current term to e
+        i++;
     }
-    printf("Approximation of e using n = %d: %.10f\n", n, e);
+    printf("Approximation of e: %.10f\n", e);
 
     return 0;
 }
