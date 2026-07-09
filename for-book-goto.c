12a6d0bf6123c7b816bef9f72b5f63021d89d282
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-10 02:28:29 +0800

    chore: replace placeholder syntax for old block of code
    chore: remove commented code from old exercise

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,8 +1,4 @@
-/*The value of the mathematical constant e can be expressed as an infinite series:
-e = 1 + 1/1! + 1/2! + 1/3! + …
-Write a program that approximates e by computing the value of
-1 + 1/1! + 1/2! + 1/3! + … + 1/n!
-where n is an integer entered by the user.*/
+
 
 /*Modify the program so that the program continues adding terms until the current term becomes less than ε, where ε is a small (floating-point) number entered by the user.
 */
@@ -10,22 +6,13 @@ where n is an integer entered by the user.*/
 #include <stdio.h>
 
 int main() {
-    int n, i;
-    double e = 1.0, factorial = 1.0, epsilon;
-    printf("Enter an integer n to approximate e: ");
-    scanf("%d", &n);
-    printf("Enter the tolerance epsilon: ");
-    scanf("%lf", &epsilon);
 
-    double term = 1.0;
-    i = 1;
-    while (term >= epsilon) {
-        factorial *= i; // Calculate i!
-        term = 1.0 / factorial; // Calculate the current term
-        e += term; // Add the current term to e
-        i++;
-    }
-    printf("Approximation of e: %.10f\n", e);
+short int
+unsigned short int
+int
+unsigned int
+long int
+unsigned long int
 
     return 0;
 }
