3df1be82d3278612983b5817bc275dbd31752e2b
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-11 03:24:18 +0800

    chore: update old commented code

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,22 +1,12 @@
-/*Modify the block of code into a basic example for short ints and ints and print the output*/
+/*Modify the block of code into a basic example for ints*/
 
 #include <stdio.h>
 
 int main() {
-
-short int a = 5;
-unsigned short int b = 10;
-int c = 15;
-unsigned int d = 20;
-long int e = 25;
-unsigned long int f = 30;  
-
-    printf("short int a = %d\n", a);
-    printf("unsigned short int b = %u\n", b);
-    printf("int c = %d\n", c);
-    printf("unsigned int d = %u\n", d);
-    printf("long int e = %ld\n", e);
-    printf("unsigned long int f = %lu\n", f);
+    
+short s;
+scanf("%hd", &s);
+printf("%hd", s);
 
     return 0;
 }
