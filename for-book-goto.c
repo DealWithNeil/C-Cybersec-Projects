3810ec5a07b5301cc4d099497e86537405e51226
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-15 02:36:43 +0800

    chore: update old commented code

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,7 +1,6 @@
 /*Display a basic program for this: DOctal constants contain only digits between 0 and 7, and must begin with a
 zero:
-017  0377  077777
-*/
+017  0377  077777*/
 
 #include <stdio.h>
 
