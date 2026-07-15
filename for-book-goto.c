ceadeedd96da6c44f15c7bb5de82cb6cc1bc39bb
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-16 03:18:41 +0800

    chore: remove old block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -8,13 +8,6 @@ printf("%hd", s);*/
 
 int main() {
     
-    int hexConstant1 = 0xf;    // Valid hexadecimal constant
-    int hexConstant2 = 0xff;   // Valid hexadecimal constant
-    int hexConstant3 = 0x7fff; // Valid hexadecimal constant
-
-    printf("Hexadecimal constant 1: %d\n", hexConstant1);
-    printf("Hexadecimal constant 2: %d\n", hexConstant2);
-    printf("Hexadecimal constant 3: %d\n", hexConstant3);
-
+    
     return 0;
 }
