b77aef4efb408d9b6e46d15714069501018e8dd4
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-18 03:15:49 +0800

    chore: update old block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -2,10 +2,17 @@
 
 #include <stdio.h>
 
-int main() {
-    long l;
-    scanf("%ld", &l);
-    printf("%ld", l);
+int main(void)
+{
+  long n, sum = 0;
+  printf("This program sums a series of integers.\n");
+   printf("Enter integers (0 to terminate): ");
+  scanf("%ld", &n);
+  while (n != 0) {
+    sum += n;
+    scanf("%ld", &n);
+  }
+  printf("The sum is: %ld\n", sum);
 
     return 0;
 }
