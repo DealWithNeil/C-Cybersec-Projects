2d0d01bf698f4f5af8654cc63ec2bc63fa1e81f9
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-24 00:15:23 +0800

    chore: update old commented code

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,5 +1,7 @@
-/*  
-*/
+/*Display a simple program for this and print it:  Unsigned integer types, both standard (unsigned char, unsigned
+short int, unsigned int, unsigned long int, unsigned
+long long int, _Bool) and extended*/
+
 #include <stdio.h>
 
 int main(void)
