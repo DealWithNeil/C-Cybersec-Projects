122faa3629c49609a028fcb33da4e90b3a0ce9ed
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-24 00:15:55 +0800

    chore: create new block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -6,5 +6,13 @@ long long int, _Bool) and extended*/
 
 int main(void)
 {
-    
+    printf("Unsigned integer types:\n");
+    printf("unsigned char: %zu bytes\n", sizeof(unsigned char));
+    printf("unsigned short int: %zu bytes\n", sizeof(unsigned short int));
+    printf("unsigned int: %zu bytes\n", sizeof(unsigned int));
+    printf("unsigned long int: %zu bytes\n", sizeof(unsigned long int));
+    printf("unsigned long long int: %zu bytes\n", sizeof(unsigned long long int));
+    printf("_Bool: %zu bytes\n", sizeof(_Bool));
+
+    return 0;
 }
