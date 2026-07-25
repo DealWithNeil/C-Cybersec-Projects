f363fe98637e8db3d662d04a6a2b88eed05dd853
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-26 01:19:43 +0800

    chore: updated block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -4,7 +4,11 @@ and Complex floating types (float _Complex, double _Complex, long double _Comple
 
 int main(void)
 {
-    printf("Complex types in C17:\n");
+    printf("Real types in C17:\n");
+    printf("float: %zu bytes\n", sizeof(float));
+    printf("double: %zu bytes\n", sizeof(double));
+    printf("long double: %zu bytes\n", sizeof(long double));
+    printf("\nComplex types in C17:\n");
     printf("float _Complex: %zu bytes\n", sizeof(float _Complex));
     printf("double _Complex: %zu bytes\n", sizeof(double _Complex));
     printf("long double _Complex: %zu bytes\n", sizeof(long double _Complex));
