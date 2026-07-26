915a84863afbc6752867bf9146695efd2ceedb93
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-27 03:50:01 +0800

    chore: update old block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -3,11 +3,6 @@
 
 int main(void)
 {
-    printf("Real types in C17:\n");
-    printf("float: %zu bytes\n", sizeof(float));
-    printf("double: %zu bytes\n", sizeof(double));
-    printf("long double: %zu bytes\n", sizeof(long double));
-    printf("\nComplex types in C17:\n");
     printf("float _Complex: %zu bytes\n", sizeof(float _Complex));
     printf("double _Complex: %zu bytes\n", sizeof(double _Complex));
     printf("long double _Complex: %zu bytes\n", sizeof(long double _Complex));
