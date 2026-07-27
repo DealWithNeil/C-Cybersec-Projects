7c4c9069d7ee603c852cfa66d17599a52b9cd3c6
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-28 00:22:21 +0800

    chore: update old commented code for newer exercise

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,4 +1,6 @@
-/*Display a simple program for this and print it:   floating types (float _Complex, double _Complex, long double _Complex)*/
+/*Display a simple program for this and print it: An octal escape sequence consists of the \ character followed by an octal
+number with at most three digits. (This number must be representable as an
+unsigned character, so its maximum value is normally 377 octal.)*/
 #include <stdio.h>
 
 int main(void)
