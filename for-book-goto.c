0c11505c284c276bfd10c4b2fa0f8e35b41496c6
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-07-29 00:30:32 +0800

    chore: update old commented code for a program with relational operators

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,6 +1,5 @@
-/*Display a simple program for this and print it: An octal escape sequence consists of the \ character followed by an octal
-number with at most three digits. (This number must be representable as an
-unsigned character, so its maximum value is normally 377 octal.)*/
+/*Display a simple program for this and print it: if ('a' <= ch && ch <= 'z')
+  ch = ch - 'a' + 'A';*/
 
 #include <stdio.h>
 
