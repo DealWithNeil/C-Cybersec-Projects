7636f699d019a5cf40ab4dd3f82f1c0841072d0e
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-03 00:05:52 +0800

    chore: add new lines of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -4,10 +4,11 @@
 
 int main(void)
 {
-  
+  printf("Type Conversion Example\n");
   int x = 5;
   double y = 2.5;
   double result = x + y; // Implicit type conversion from int to double
+  printf("Result: %f\n", result); 
 
   return 0;
 }
