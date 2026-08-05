cf13288aa04f96c52f7f594febb5807c7cc160d4
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-06 00:01:36 +0800

    chore: print the output

--- for-book-goto.c
+++ for-book-goto.c
@@ -12,6 +12,7 @@ double d;
 i = c;   /* c is converted to int   */
 f = i;   /* i is converted to float */
 d = f;   /* f is converted to double */
-    
+printf("c = %c, i = %d, f = %f, d = %lf\n", c, i, f, d);
+
     return 0;
 }
