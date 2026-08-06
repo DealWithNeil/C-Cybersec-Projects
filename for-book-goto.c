7aa888f9dc24e891480f5ba37f53de2f1eb24677
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-07 00:30:40 +0800

    chore: remove old block of code for cast expression

--- for-book-goto.c
+++ for-book-goto.c
@@ -5,14 +5,5 @@
 int main(void)
 {
     
-char c;
-int i;
-float f;
-double d;
-i = c;   /* c is converted to int   */
-f = i;   /* i is converted to float */
-d = f;   /* f is converted to double */
-printf("c = %c, i = %d, f = %f, d = %lf\n", c, i, f, d);
-
     return 0;
 }
