5f63143cfafbf5ccba81319672113b473dd131ad
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-10 00:13:34 +0800

    chore: update old commented code

--- for-book-goto.c
+++ for-book-goto.c
@@ -4,9 +4,18 @@
 
 int main(void)
 {
-    int i = 10;
+    typedef float Dollars;
 
-    printf("The value of i is: %d\n", i);
+    Dollars amount = 100.50;
+    printf("The value of amount is: %.2f\n", amount);
 
+    Dollars cash_in, cash_out;
+
+    cash_in = 50.25;
+    cash_out = 20.75;
+    printf("The value of cash_in is: %.2f\n", cash_in);
+    printf("The value of cash_out is: %.2f\n", cash_out);
+
+    
     return 0;
 }
