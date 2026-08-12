6370bf11644b01029806af972abc4c66708c738c
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-13 01:16:32 +0800

    chore: update old block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -6,6 +6,10 @@ int main(void)
 {
     int students[20];
     printf("%zu", sizeof(students)); // 80 bytes 
+
+    int *ptr = (int *)malloc(20 * sizeof(int));
+    int *ptr2 = (int *)calloc(20, sizeof(int));
+    
     
     return 0;
 }
