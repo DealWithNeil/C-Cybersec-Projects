6ce688bb43bd5269b8482b39b40abcb383209d12
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-15 00:17:36 +0800

    chore: update old block of code

--- for-book-goto.c
+++ for-book-goto.c
@@ -2,23 +2,40 @@
 
 #include <stdio.h>
 
-#define NAME_LEN 25
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_NAME_LEN 50
 
 int main(void)
 {
-    /* Anonymous struct definition declaring two variables: part1 and part2 */
+    /* Anonymous struct definition declaring two variables: emp1 and emp2 */
     struct {
-        int number;
-        char name[NAME_LEN + 1];
-        int on_hand;
-    } part1, part2;
+        int employee_id;
+        char name[MAX_NAME_LEN + 1];
+        float salary;
+    } emp1, emp2;
 
-    /* Example initialization / usage */
-    part1.number = 101;
-    part1.on_hand = 50;
+    /* Initializing emp1 */
+    emp1.employee_id = 4051;
+    strcpy(emp1.name, "Alice Smith");
+    emp1.salary = 75500.50;
 
-    printf("Part 1 Number: %d\n", part1.number);
-    printf("Part 1 On Hand: %d\n", part1.on_hand);
+    /* Initializing emp2 */
+    emp2.employee_id = 4052;
+    strcpy(emp2.name, "Bob Jones");
+    emp2.salary = 68000.00;
+
+    /* Printing the data */
+    printf("--- Employee 1 ---\n");
+    printf("ID: %d\n", emp1.employee_id);
+    printf("Name: %s\n", emp1.name);
+    printf("Salary: $%.2f\n\n", emp1.salary);
+
+    printf("--- Employee 2 ---\n");
+    printf("ID: %d\n", emp2.employee_id);
+    printf("Name: %s\n", emp2.name);
+    printf("Salary: $%.2f\n", emp2.salary);
 
     return 0;
 }
