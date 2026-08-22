9d3497616dc90b9625edeaf07c79b2614d3f3650
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-23 01:26:57 +0800

    chore: updated the old commented code

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,4 +1,4 @@
-/* Display and print a basic structure variable program:domain and context, macro name and value, variable name, print formatting.*/
+/* Display and print a basic structure variable program: updated the domain and context, macro name and value, variable name, print formatting.*/
 
 #include <stdio.h>
 #include <string.h>
