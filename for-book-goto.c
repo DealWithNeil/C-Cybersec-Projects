64fa90c87134d6ac66a761bc10e43f4f21836155
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-27 00:16:33 +0800

    chore: update old commented code

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,4 +1,4 @@
-/* Display and print a basic structure variable program: add conference event example using anonymous struct.*/
+/* Display and print a basic structure variable program: add restaurant order example using anonymous struct.*/
 
 #include <stdio.h>
 #include <string.h>
