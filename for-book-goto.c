d8cb9c9479104541f125b042c6dfb9e346a14e89
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-29 00:08:51 +0800

    feat: add book catalog example using anonymous struct

--- for-book-goto.c
+++ for-book-goto.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 
-#define ASSET_LEN 30
+#define AUTHOR_LEN 40
 
 int main(void)
 {
-    /* Anonymous struct declaring variable gadget1 */
+    /* Anonymous struct declaring variable book1 */
     struct {
-        int warranty_months;
-        char asset_tag[ASSET_LEN + 1];
-        double replacement_cost;
-    } gadget1;
+        int page_count;
+        char author[AUTHOR_LEN + 1];
+        double retail_price;
+    } book1;
 
-    /* Assigning values to gadget1 members */
-    gadget1.warranty_months = 24;
-    strcpy(gadget1.asset_tag, "Dell XPS 15 Laptop");
-    gadget1.replacement_cost = 1799.99;
+    /* Assigning values to book1 members */
+    book1.page_count = 416;
+    strcpy(book1.author, "George Orwell");
+    book1.retail_price = 14.99;
 
-    /* Printing the contents of gadget1 */
-    printf("=== IT Asset Details ===\n");
-    printf("Asset Tag:    %s\n", gadget1.asset_tag);
-    printf("Warranty:     %d months\n", gadget1.warranty_months);
-    printf("Value:        $%.2f\n", gadget1.replacement_cost);
+    /* Printing the contents of book1 */
+    printf("=== Book Catalog Information ===\n");
+    printf("Author:       %s\n", book1.author);
+    printf("Page Count:   %d pages\n", book1.page_count);
+    printf("Retail Price: $%.2f\n", book1.retail_price);
 
     return 0;
 }
