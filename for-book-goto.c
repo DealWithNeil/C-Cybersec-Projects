53a03255247c72ee02622b8a8779ed4638c2a145
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-31 00:45:59 +0800

    feat: add workout log example using anonymous struct

--- for-book-goto.c
+++ for-book-goto.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 
-#define PLANET_LEN 30
+#define EXERCISE_LEN 35
 
 int main(void)
 {
-    /* Anonymous struct declaring variable body1 */
+    /* Anonymous struct declaring variable workout1 */
     struct {
-        int moon_count;
-        char planet_name[PLANET_LEN + 1];
-        double distance_from_sun_au;
-    } body1;
+        int set_count;
+        char exercise_name[EXERCISE_LEN + 1];
+        double weight_kg;
+    } workout1;
 
-    /* Assigning values to body1 members */
-    body1.moon_count = 95;
-    strcpy(body1.planet_name, "Jupiter");
-    body1.distance_from_sun_au = 5.20;
+    /* Assigning values to workout1 members */
+    workout1.set_count = 4;
+    strcpy(workout1.exercise_name, "Barbell Bench Press");
+    workout1.weight_kg = 82.5;
 
-    /* Printing the contents of body1 */
-    printf("=== Astronomical Data ===\n");
-    printf("Planet Name: %s\n", body1.planet_name);
-    printf("Moons:       %d\n", body1.moon_count);
-    printf("Distance:    %.2f AU\n", body1.distance_from_sun_au);
+    /* Printing the contents of workout1 */
+    printf("=== Fitness Log Details ===\n");
+    printf("Exercise: %s\n", workout1.exercise_name);
+    printf("Sets:     %d\n", workout1.set_count);
+    printf("Weight:   %.1f kg\n", workout1.weight_kg);
 
     return 0;
 }
