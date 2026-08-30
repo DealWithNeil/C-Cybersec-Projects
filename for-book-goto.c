26367927eb84994eb81d3f95cf998d18eba335bf
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-08-30 17:26:09 +0800

    chore: add astronomical data example using anonymous struct

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,4 +1,4 @@
-/* Display and print a basic structure variable program: add book catalog example using anonymous struct.*/
+/* Display and print a basic structure variable program: add astronomical data example using anonymous struct.*/
 
 #include <stdio.h>
 #include <string.h>
