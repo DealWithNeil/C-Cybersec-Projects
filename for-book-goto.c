234fe0daaf9db32de3b652cbb4faf98bea886e50
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-09-01 00:27:35 +0800

    chore: add zoo exhibit record example using anonymous struct

--- for-book-goto.c
+++ for-book-goto.c
@@ -1,4 +1,4 @@
-/* Display and print a basic structure variable program: add workout log example using anonymous struct.*/
+/* Display and print a basic structure variable program: add zoo exhibit record example using anonymous struct.*/
 
 #include <stdio.h>
 #include <string.h>
