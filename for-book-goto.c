7140c3882b6f7c10bdd5524e6ac1d1b78954a652
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-09-03 04:57:42 +0800

    feat: add package shipment example using anonymous struct

--- for-book-goto.c
+++ for-book-goto.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 #include <string.h>
 
-#define DRINK_LEN 30
+#define CARRIER_LEN 30
 
 int main(void)
 {
-    /* Anonymous struct declaring variable beverage1 */
+    /* Anonymous struct declaring variable shipment1 */
     struct {
-        int calories;
-        char drink_name[DRINK_LEN + 1];
-        double size_ounces;
-    } beverage1;
+        int delivery_days;
+        char carrier_name[CARRIER_LEN + 1];
+        double weight_lbs;
+    } shipment1;
 
-    /* Assigning values to beverage1 members */
-    beverage1.calories = 190;
-    strcpy(beverage1.drink_name, "Iced Matcha Latte");
-    beverage1.size_ounces = 16.0;
+    /* Assigning values to shipment1 members */
+    shipment1.delivery_days = 3;
+    strcpy(shipment1.carrier_name, "FedEx Express");
+    shipment1.weight_lbs = 12.4;
 
-    /* Printing the contents of beverage1 */
-    printf("=== Cafe Menu Item ===\n");
-    printf("Item Name: %s\n", beverage1.drink_name);
-    printf("Size:      %.1f oz\n", beverage1.size_ounces);
-    printf("Calories:  %d kcal\n", beverage1.calories);
+    /* Printing the contents of shipment1 */
+    printf("=== Package Shipment Record ===\n");
+    printf("Carrier:       %s\n", shipment1.carrier_name);
+    printf("Weight:        %.1f lbs\n", shipment1.weight_lbs);
+    printf("Delivery Time: %d days\n", shipment1.delivery_days);
 
     return 0;
 }
