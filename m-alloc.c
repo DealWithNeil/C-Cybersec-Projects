e18959e59e0b3d226675cacd480f7fbcd4303d9e
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-09-09 15:21:49 +0800

    create: new program for memory allocator

---
+++ m-alloc.c
@@ -0,0 +1,28 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    /* Dynamically allocate memory for a single integer */
+    int *num = malloc(sizeof(int));
+
+    /* Check if the memory allocation was successful */
+    if (num == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    /* Assign a value to the allocated memory */
+    *num = 42;
+
+    /* Print the value stored at the allocated address */
+    printf("Value: %d\n", *num);
+
+    /* Free the allocated memory and reset pointer */
+    free(num);
+    num = NULL;
+
+    return 0;
+}
