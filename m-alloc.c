ab7bbad0da9952390aa650cc3bf37c08bd1640c6
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-09-14 00:04:47 +0800

    create: new line of directives

--- m-alloc.c
+++ m-alloc.c
@@ -1,31 +1,9 @@
-//Memory allocation: add dynamic string allocation example using malloc and strcpy
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
 
-#define STRING_LEN 50
+#define HEAP_SIZE 1024
+#define ALIGNMENT 8
 
-int main(void)
-{
-    /* Dynamically allocate memory for a string (char array) */
-    char *message = malloc((STRING_LEN + 1) * sizeof(char));
-
-    /* Check if memory allocation succeeded */
-    if (message == NULL) {
-        printf("Memory allocation failed!\n");
-        return 1;
-    }
-
-    /* Assign string data using strcpy */
-    strcpy(message, "Dynamic memory allocation for strings in C!");
-
-    /* Print the contents of the allocated string */
-    printf("Message: %s\n", message);
-
-    /* Free the allocated memory block and reset pointer */
-    free(message);
-    message = NULL;
-
-    return 0;
-}
