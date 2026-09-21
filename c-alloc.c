f5579e4f6d85db053734004c91943748895b401a
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-09-22 00:47:03 +0800

    feat: Demonstrates that realloc works in both directions not just growing an array, but also truncating it to free up unused space.

--- c-alloc.c
+++ c-alloc.c
@@ -3,21 +3,8 @@
 
 int main(void)
 {
-    /* 1. Allocate initial memory for 2 integers */
-    int *arr = malloc(2 * sizeof(int));
-    arr[0] = 10;
-    arr[1] = 20;
-
-    /* 2. Resize the array to hold 4 integers */
-    arr = realloc(arr, 4 * sizeof(int));
-    arr[2] = 30;
-    arr[3] = 40;
-
-    /* Print all 4 elements */
-    printf("%d %d %d %d\n", arr[0], arr[1], arr[2], arr[3]);
-
-    /* 3. Free memory */
-    free(arr);
-
-    return 0;
-}
+    
+    int *numbers = malloc(3 * sizeof(int));
+    numbers[0] = 100;
+    numbers[1] = 200;
+    numbers[2] = 300;
