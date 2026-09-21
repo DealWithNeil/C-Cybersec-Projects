6c6eaefdbef71782d5dc5ef38938a7c98dcdda7b
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-09-22 00:47:31 +0800

    feat(memory): demonstrate reducing array size with realloc

--- c-alloc.c
+++ c-alloc.c
@@ -8,3 +8,12 @@ int main(void)
     numbers[0] = 100;
     numbers[1] = 200;
     numbers[2] = 300;
+
+    numbers = realloc(numbers, 2 * sizeof(int));
+
+    printf("%d %d\n", numbers[0], numbers[1]);
+
+    free(numbers);
+
+    return 0;
+}
