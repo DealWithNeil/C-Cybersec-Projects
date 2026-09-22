d62b93bf7b857587e9890907fe7c50f3b869dcf9
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-09-23 00:21:34 +0800

    feat: Add clean realloc example resizing double precision float array

--- c-alloc.c
+++ c-alloc.c
@@ -6,3 +6,13 @@ int main(void)
     double *prices = malloc(2 * sizeof(double));
     prices[0] = 19.99;
     prices[1] = 49.50;
+    
+    prices = realloc(prices, 3 * sizeof(double));
+    prices[2] = 89.95;
+
+    printf("%.2f %.2f %.2f\n", prices[0], prices[1], prices[2]);
+
+    free(prices);
+
+    return 0;
+}
